4d9b34941b29a9178f4eb7a887eb241956b2ea13
medic-code <u40as7@gmail.com>
2025-08-16 18:42:49 +0100

    Chore: fix lint error in sample e2e

--- tests/e2e/sample.spec.js
+++ tests/e2e/sample.spec.js
@@ -1,7 +1,6 @@
 /* write e2e specs here */
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('app boots', async({ page, baseURL }) => {
     await page.goto(baseURL);
-    // Adjust the selector to something you know will exist
 });
