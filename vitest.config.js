a604323f9cca49c64959441d89d0a6edf14e81b7
medic-code <u40as7@gmail.com>
2025-08-16 19:05:46 +0100

    chore: fix e2e webserver not starting for ci - plus minor eslint fixes

--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,15 @@
 import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'happy-dom',
-        include: ['tests/unit/**/*.spec.js', 'tests/dom/**/*.spec.js'],
-        exclude: [...configDefaults.exclude, 'tests/e2e/**'],
-        setupFiles: './tests/setup.js',
-        converage: {
-            provider: 'v8',
-            reports: ['text', 'html'],
-        },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: ['tests/unit/**/*.spec.js', 'tests/dom/**/*.spec.js'],
+    exclude: [...configDefaults.exclude, 'tests/e2e/**'],
+    setupFiles: './tests/setup.js',
+    converage: {
+      provider: 'v8',
+      reports: ['text', 'html'],
     },
+  },
 });
